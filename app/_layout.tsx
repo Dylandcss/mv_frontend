755303726d91da6feec61b9424a89499f2b6dd4b
dylan <decmps.dylan@gmail.com>
2025-04-02 17:31:47 +0200

    Améliorations UI

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,8 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return <Stack>
     <Stack.Screen name="index" options={{ headerShown: false }} />
+    <Stack.Screen name="home" options={{ headerShown: false }} />
+    <Stack.Screen name="login" options={{ headerShown: false }} />
     <Stack.Screen name="addVisite" options={{ title: "Ajouter une visite" }} />
     <Stack.Screen name="viewVisites" options={{ title: "Voir les visites" }} />
   </Stack>;
